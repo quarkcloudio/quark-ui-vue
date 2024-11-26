9d6b3cc283b49df3d76847b16949de7c2ca70ea0
tangtanglove <dai_hang_love@126.com>
2024-11-26 15:41:57 +0800

    chore: wip

--- src/components/Form/index.vue
+++ src/components/Form/index.vue
@@ -1,17 +1,4 @@
 <script lang="ts" setup>
-import { reactive } from 'vue';
-
-interface FormState {
-  username: string;
-  password: string;
-  remember: boolean;
-}
-
-const formState = reactive<FormState>({
-  username: '',
-  password: '',
-  remember: true,
-});
 const onFinish = (values: any) => {
   console.log('Success:', values);
 };
@@ -23,7 +10,6 @@ const onFinishFailed = (errorInfo: any) => {
 
 <template>
   <a-form
-    :model="formState"
     name="basic"
     :label-col="{ span: 8 }"
     :wrapper-col="{ span: 16 }"
@@ -31,12 +17,6 @@ const onFinishFailed = (errorInfo: any) => {
     @finish="onFinish"
     @finishFailed="onFinishFailed"
   >
-    <a-form-item name="remember" :wrapper-col="{ offset: 8, span: 16 }">
-      <a-checkbox v-model:checked="formState.remember">Remember me</a-checkbox>
-    </a-form-item>
 
-    <a-form-item :wrapper-col="{ offset: 8, span: 16 }">
-      <a-button type="primary" html-type="submit">Submit</a-button>
-    </a-form-item>
   </a-form>
 </template>
