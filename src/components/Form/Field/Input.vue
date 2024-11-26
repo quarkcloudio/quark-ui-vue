2c8a725a26489d06eadf463cf1ced1938fc3373c
tangtanglove <dai_hang_love@126.com>
2024-11-26 10:40:20 +0800

    chore: wip

---
+++ src/components/Form/Field/Input.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const props = withDefaults(defineProps<{
+  addonAfter?: any;
+  addonBefore?: any;
+  allowClear?: boolean;
+}>(), {
+  addonAfter: undefined,
+  addonBefore: undefined,
+  allowClear: undefined,
+});
+
+const { addonAfter, addonBefore, allowClear } = toRefs(props)
+</script>
+<template>
+  <a-input
+    :addonAfter="addonAfter"
+    :addonBefore="addonBefore"
+    :allowClear="allowClear"
+  >
+  
+  </a-input>
+</template>
