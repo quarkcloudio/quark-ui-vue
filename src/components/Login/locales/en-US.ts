d7874eb02a81dd83c14aed9e7c9369390e660f40
tangtanglove <dai_hang_love@126.com>
2024-12-13 11:35:55 +0800

    chore: wip

--- src/components/Login/locales/en-US.ts
+++ src/components/Login/locales/en-US.ts
@@ -3,7 +3,4 @@ export default {
   'login.rememberMe': 'Remember me',
   'login.forgotPassword': 'Forgot Password ?',
   'login.submit': 'Login',
-  'login.loginWith': 'Login with :',
-  'login.registerAccount': 'Register Account',
-  'login.tips': 'welcome to the system',
 }
--- src/components/Login/locales/zh-CN.ts
+++ src/components/Login/locales/zh-CN.ts
@@ -3,7 +3,4 @@ export default {
   'login.rememberMe': '自动登录',
   'login.forgotPassword': '忘记密码 ?',
   'login.submit': '登录',
-  'login.loginWith': '其他登录方式 :',
-  'login.registerAccount': '注册账户',
-  'login.tips': '欢迎使用本系统',
 }
