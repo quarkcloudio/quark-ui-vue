ab5005b9d81f1488007ed0f882aef76675659dc5
tangtanglove <dai_hang_love@126.com>
2025-08-22 17:27:00 +0800

    chore: wip

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,15 +1,11 @@
 <script setup lang="ts">
-import HeaderBanner from './modules/header-banner.vue';
 import CardData from './modules/card-data.vue';
 import LineChart from './modules/line-chart.vue';
 import PieChart from './modules/pie-chart.vue';
-import ProjectNews from './modules/project-news.vue';
-import CreativityBanner from './modules/creativity-banner.vue';
 </script>
 
 <template>
   <ASpace direction="vertical" :size="16">
-    <HeaderBanner />
     <CardData />
     <ARow :gutter="[16, 16]">
       <ACol :span="24" :lg="14">
@@ -17,14 +13,6 @@ import CreativityBanner from './modules/creativity-banner.vue';
       </ACol>
       <ACol :span="24" :lg="10">
         <PieChart />
-      </ACol>
-    </ARow>
-    <ARow :gutter="[16, 16]">
-      <ACol :span="24" :lg="14">
-        <ProjectNews />
-      </ACol>
-      <ACol :span="24" :lg="10">
-        <CreativityBanner />
       </ACol>
     </ARow>
   </ASpace>
