6d333493a73b5ee6fbe08066c094b061800d58fb
tangtanglove <dai_hang_love@126.com>
2025-08-24 13:44:45 +0800

    chore: wip

--- src/views/manage/role/modules/role-search.vue
+++ src/views/manage/role/modules/role-search.vue
@@ -25,49 +25,51 @@ function search() {
 </script>
 
 <template>
-  <ACard :title="$t('common.search')" :bordered="false" class="card-wrapper">
-    <AForm :model="model" :label-width="80">
-      <ARow :gutter="[16, 16]" wrap>
-        <ACol :span="24" :md="12" :lg="6">
-          <AFormItem :label="$t('page.manage.role.roleName')" name="roleName" class="m-0">
-            <AInput v-model:value="model.roleName" :placeholder="$t('page.manage.role.form.roleName')" />
-          </AFormItem>
-        </ACol>
-        <ACol :span="24" :md="12" :lg="6">
-          <AFormItem :label="$t('page.manage.role.roleCode')" name="roleCode" class="m-0">
-            <AInput v-model:value="model.roleCode" :placeholder="$t('page.manage.role.form.roleCode')" />
-          </AFormItem>
-        </ACol>
-        <ACol :span="24" :md="12" :lg="6">
-          <AFormItem :label="$t('page.manage.role.roleStatus')" name="status" class="m-0">
-            <ASelect v-model:value="model.status" :placeholder="$t('page.manage.role.form.roleStatus')" allow-clear>
-              <ASelectOption v-for="option in enableStatusOptions" :key="option.value" :value="option.value">
-                {{ $t(option.label) }}
-              </ASelectOption>
-            </ASelect>
-          </AFormItem>
-        </ACol>
-        <ACol :span="24" :md="12" :lg="6">
-          <AFormItem class="m-0">
-            <div class="w-full flex-y-center justify-end gap-12px">
-              <AButton @click="reset">
-                <template #icon>
-                  <icon-ic-round-refresh class="align-sub text-icon" />
-                </template>
-                <span class="ml-8px">{{ $t('common.reset') }}</span>
-              </AButton>
-              <AButton type="primary" ghost @click="search">
-                <template #icon>
-                  <icon-ic-round-search class="align-sub text-icon" />
-                </template>
-                <span class="ml-8px">{{ $t('common.search') }}</span>
-              </AButton>
-            </div>
-          </AFormItem>
-        </ACol>
-      </ARow>
-    </AForm>
-  </ACard>
+  <ACollapse :bordered="false" default-active-key="1" class="card-wrapper" style="background: rgb(255, 255, 255)">
+    <ACollapsePanel key="1" :header="$t('common.search')">
+      <AForm :model="model" :label-width="80">
+        <ARow :gutter="[16, 16]" wrap>
+          <ACol :span="24" :md="12" :lg="6">
+            <AFormItem :label="$t('page.manage.role.roleName')" name="roleName" class="m-0">
+              <AInput v-model:value="model.roleName" :placeholder="$t('page.manage.role.form.roleName')" />
+            </AFormItem>
+          </ACol>
+          <ACol :span="24" :md="12" :lg="6">
+            <AFormItem :label="$t('page.manage.role.roleCode')" name="roleCode" class="m-0">
+              <AInput v-model:value="model.roleCode" :placeholder="$t('page.manage.role.form.roleCode')" />
+            </AFormItem>
+          </ACol>
+          <ACol :span="24" :md="12" :lg="6">
+            <AFormItem :label="$t('page.manage.role.roleStatus')" name="status" class="m-0">
+              <ASelect v-model:value="model.status" :placeholder="$t('page.manage.role.form.roleStatus')" allow-clear>
+                <ASelectOption v-for="option in enableStatusOptions" :key="option.value" :value="option.value">
+                  {{ $t(option.label) }}
+                </ASelectOption>
+              </ASelect>
+            </AFormItem>
+          </ACol>
+          <ACol :span="24" :md="12" :lg="6">
+            <AFormItem class="m-0">
+              <div class="w-full flex-y-center justify-end gap-12px">
+                <AButton @click="reset">
+                  <template #icon>
+                    <icon-ic-round-refresh class="align-sub text-icon" />
+                  </template>
+                  <span class="ml-8px">{{ $t('common.reset') }}</span>
+                </AButton>
+                <AButton type="primary" ghost @click="search">
+                  <template #icon>
+                    <icon-ic-round-search class="align-sub text-icon" />
+                  </template>
+                  <span class="ml-8px">{{ $t('common.search') }}</span>
+                </AButton>
+              </div>
+            </AFormItem>
+          </ACol>
+        </ARow>
+      </AForm>
+    </ACollapsePanel>
+  </ACollapse>
 </template>
 
 <style scoped></style>
