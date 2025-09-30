ec6b7d5e1a9a4daa1992886100bb3104c33d28b4
tangtanglove <dai_hang_love@126.com>
2025-09-30 18:28:29 +0800

    chore: 优化Table组件

--- src/components/pro-table/pro-table.vue
+++ src/components/pro-table/pro-table.vue
@@ -1,6 +1,6 @@
 <script setup lang="tsx">
-import { ref, toRefs, watchEffect } from 'vue';
-import type { TableProps } from 'ant-design-vue';
+import { ref, toRefs, watch } from 'vue';
+import type { TableColumnType, TableProps } from 'ant-design-vue';
 import { Space } from 'ant-design-vue';
 import Render from '@/components/render/index.vue';
 import { useEngineStore } from '@/store/modules/engine';
@@ -9,11 +9,11 @@ import Action from '@/components/action/index.vue';
 
 interface ProTableProps {
   rowKey: string;
-  columns: any[];
-  datasource?: any[];
+  columns: TableColumnType[];
+  datasource?: Record<string, any>[];
   headerTitle?: string;
-  search?: any;
-  toolBar?: any;
+  search?: Record<string, any>;
+  toolBar?: Record<string, any>;
   pagination?: any;
 }
 
@@ -24,157 +24,159 @@ defineOptions({
 const props = defineProps<ProTableProps>();
 const { rowKey, columns, headerTitle, search, toolBar } = toRefs(props);
 const { engineApi } = useEngineStore();
-const datasource = ref<any[]>(props.datasource || []);
+
+const datasource = ref<Record<string, any>[]>(props.datasource || []);
 const loading = ref(false);
 
-const getColumnChecks = () => {
-  const checks: AntDesign.TableColumnCheck[] = [];
-  columns.value.forEach(column => {
-    if (column.dataIndex) {
-      checks.push({
-        key: column.dataIndex as string,
-        title: column.title as string,
-        checked: true
-      });
-    }
-  });
-  return checks;
-};
+// 列选择器
+const getColumnChecks = () =>
+  columns.value
+    .filter(col => col.dataIndex)
+    .map(col => ({
+      key: col.dataIndex as string,
+      title: col.title as string,
+      checked: true
+    }));
 
-const columnChecks = ref<any[]>(getColumnChecks() || []);
+const columnChecks = ref<any[]>(getColumnChecks());
 
+// 渲染列
 const getColumns = () => {
   const columnMap = new Map<string, any>();
-
-  columns.value.forEach(column => {
-    if (column.dataIndex) {
-      columnMap.set(column.dataIndex as string, column);
+  columns.value.forEach(col => {
+    if (col.dataIndex) {
+      columnMap.set(col.dataIndex as string, col);
     }
   });
 
-  const filteredColumns = columnChecks?.value?.filter(item => item.checked).map(check => columnMap.get(check.key));
+  return columnChecks.value
+    ?.filter(item => item.checked)
+    .map(check => {
+      const col = { ...columnMap.get(check.key) };
 
-  return filteredColumns?.map((item: any) => {
-    const column: any = item;
-
-    // 解析筛选项
-    if (column.filters) {
-      column.filters = column?.fieldProps?.options?.map((option: any) => ({
-        text: option.label,
-        value: option.value
-      }));
-    }
-
-    // 解析渲染
-    column.customRender = ({ text, record }: any) => {
-      if (column.valueType === 'radio' || column.valueType === 'select') {
-        return column.valueEnum[text];
+      // 解析筛选项
+      if (col.filters) {
+        col.filters = col?.fieldProps?.options?.map((opt: any) => ({
+          text: opt.label,
+          value: opt.value
+        }));
       }
-      if (column.valueType === 'option') {
-        return (
-          <Space>
-            {column?.actions?.map((action: any) => {
-              return <Action {...action} data={record} />;
-            })}
-          </Space>
-        );
-      }
-      return <Render body={text} />;
-    };
 
-    return column;
-  });
+      // 自定义渲染
+      col.customRender = ({ text, record }: any) => {
+        switch (col.valueType) {
+          case 'radio':
+          case 'select':
+            return col.valueEnum?.[text] ?? text;
+          case 'option':
+            return <Space>{col?.actions?.map((action: any) => <Action {...action} data={record} />)}</Space>;
+          default:
+            return <Render body={text} />;
+        }
+      };
+
+      return col;
+    });
 };
 
-const parsedColumns = ref<any[]>(getColumns() || []);
+const parsedColumns = ref<any[]>(getColumns());
 const selectedRowKeys = ref<any[]>([]);
 
 const pagination = ref<any>({
   total: props.pagination?.total || 0,
   current: props.pagination?.current || 1,
-  pageSize: props.pagination?.pageSize || 10
+  pageSize: props.pagination?.pageSize || 10,
+  showSizeChanger: true
 });
 
 const queryParams = ref<any>({
   filters: {},
   sorter: {},
   search: {},
-  pagination: {}
+  pagination: {
+    current: pagination.value?.current,
+    pageSize: pagination.value?.pageSize
+  }
 });
 
-watchEffect(() => {
-  parsedColumns.value = getColumns();
-});
+// 监听列变化
+watch(
+  [columns, columnChecks],
+  () => {
+    parsedColumns.value = getColumns();
+  },
+  { deep: true }
+);
 
+// 监听 queryParams 自动请求
+watch(
+  queryParams,
+  async newVal => {
+    if (loading.value) return;
+    loading.value = true;
+    try {
+      const { data }: any = await fetchTableData(engineApi, {
+        filters: JSON.stringify(newVal.filters),
+        sorter: JSON.stringify(newVal.sorter),
+        search: JSON.stringify({ ...newVal.search, ...newVal.pagination })
+      });
+      datasource.value = data?.datasource || [];
+      selectedRowKeys.value = [];
+      pagination.value = { ...pagination.value, ...data.pagination };
+    } finally {
+      loading.value = false;
+    }
+  },
+  { deep: true, immediate: true }
+);
+
+// 行选择
 const onSelectChange = (newSelectedRowKeys: any[]) => {
   selectedRowKeys.value = newSelectedRowKeys;
 };
 
-const onRequest = async () => {
-  loading.value = true;
-  const { data }: any = await fetchTableData(engineApi, {
-    filters: JSON.stringify(queryParams.value.filters),
-    sorter: JSON.stringify(queryParams.value.sorter),
-    search: JSON.stringify({ ...queryParams.value.search, ...queryParams.value.pagination })
-  });
-  datasource.value = data?.datasource || [];
-  selectedRowKeys.value = [];
-  loading.value = false;
-  return data;
-};
-
-const handleTableChange: TableProps['onChange'] = async (page: any, filters: any, sorter: any) => {
+// 表格交互改变
+const handleTableChange: TableProps['onChange'] = (page, filters, sorter) => {
   queryParams.value = {
     ...queryParams.value,
     pagination: page,
     filters,
     sorter
   };
-  const data = await onRequest();
-  pagination.value = { ...data.pagination, current: page.current };
 };
 
-const onSearch = async (values: any) => {
+// 搜索
+const onSearch = (values: any) => {
   queryParams.value = {
     ...queryParams.value,
-    pagination: {
-      current: 1,
-      pageSize: 10
-    },
+    pagination: { current: 1, pageSize: pagination.value?.pageSize || 10 },
     search: values
   };
-
-  const data = await onRequest();
-  pagination.value = { ...data.pagination };
 };
 
-const onReset = async () => {
+// 重置
+const onReset = () => {
   queryParams.value = {
     filters: {},
     sorter: {},
     search: {},
-    pagination: {
-      current: 1,
-      pageSize: 10
-    }
+    pagination: { current: 1, pageSize: pagination.value?.pageSize || 10 }
   };
-
-  const data = await onRequest();
-  pagination.value = { ...data.pagination };
 };
 
-const onExport = async () => {
-  await onRequest();
+// 导出（这里只是触发请求，可以扩展成下载逻辑）
+const onExport = () => {
+  queryParams.value = { ...queryParams.value };
 };
 </script>
 
 <template>
-  <ProTableSearch :v-if="search?.items" v-bind="search" @search="onSearch" @reset="onReset" @export="onExport" />
+  <ProTableSearch v-if="search?.items" v-bind="search" @search="onSearch" @reset="onReset" @export="onExport" />
   <ACard :title="headerTitle" class="mt-16px">
     <template #extra>
       <div class="flex items-center gap-x-12px py-12px">
-        <ProTableToolBar :actions="toolBar?.actions" :selected-row-keys="selectedRowKeys" @refresh="onRequest" />
-        <ProTableHeaderOperation v-model:columns="columnChecks" :loading="loading" @refresh="onRequest" />
+        <ProTableToolBar :actions="toolBar?.actions" :selected-row-keys="selectedRowKeys" @refresh="onReset" />
+        <ProTableHeaderOperation v-model:columns="columnChecks" :loading="loading" @refresh="onReset" />
       </div>
     </template>
     <ATable
