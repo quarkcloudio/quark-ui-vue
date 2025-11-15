4a0d1ba3fb951292ca38651922c5a79d23d81a85
tangtanglove <dai_hang_love@126.com>
2025-11-15 14:03:18 +0800

    chore: 完善ProFormFile组件

--- src/components/pro-form/pro-field/pro-form-file.vue
+++ src/components/pro-form/pro-field/pro-form-file.vue
@@ -1,43 +1,108 @@
-<script setup lang="tsx">
-import { toRefs } from 'vue';
-import Form from 'ant-design-vue/es/form/Form';
+<script setup>
+import { ref } from 'vue';
+import { UploadOutlined } from '@ant-design/icons-vue';
+import { message } from 'ant-design-vue';
+import { localStg } from '@/utils/storage';
 
 defineOptions({
   name: 'ProFormFile'
 });
 
-// 定义 props
-const props = defineProps<{
-  value?: any;
-  fieldProps?: any;
-}>();
+const props = defineProps({
+  action: String,
+  button: String,
+  limitNum: Number,
+  limitSize: Number,
+  limitType: Array,
+  onChange: Function,
+  value: {
+    type: Array,
+    default: () => []
+  }
+});
 
-// 定义 emits
-const emit = defineEmits<{
-  (e: 'update:value', value: any): void;
-}>();
+const getFileList = ref([]);
+const uploadIcon = ref(UploadOutlined);
 
-const formItemContext = Form.useInjectFormItemContext();
+const triggerChange = changedValue => {
+  if (props.onChange) {
+    props.onChange(changedValue);
+  }
+};
 
-// 初始化变量
-const { fieldProps, value } = toRefs(props);
+const onFileListChange = info => {
+  let fileList = info.fileList;
 
-// 更新字段值
-const updateValue = (newValue: any) => {
-  emit('update:value', newValue);
-  formItemContext.onFieldChange();
+  // 限制上传数量
+  fileList = fileList.slice(-props.limitNum);
+
+  // 只保存上传成功的数据
+  fileList = fileList.filter(file => {
+    if (file.response) {
+      return file.response.type === 'success';
+    }
+    return file.status === 'done';
+  });
+
+  // 重组数据
+  fileList = fileList.map(file => {
+    if (file.response) {
+      file.id = file.response.data.id;
+      file.name = file.response.data.name;
+      file.url = file.response.data.url;
+      file.size = file.response.data.size;
+    }
+    return file;
+  });
+
+  getFileList.value = fileList;
+  triggerChange(fileList);
+};
+
+const checkUpload = file => {
+  let canUpload = false;
+
+  console.log('当前文件类型：', file.type);
+  for (const type of props.limitType) {
+    if (file.type.includes(type)) {
+      canUpload = true;
+      break; // 找到匹配项后提前退出循环
+    }
+  }
+
+  if (!canUpload) {
+    message.error('请上传正确格式的文件!');
+    return false;
+  }
+
+  const isLtSize = file.size / 1024 / 1024 < props.limitSize;
+  if (!isLtSize) {
+    message.error(`文件大小不可超过${props.limitSize}MB!`);
+    return false;
+  }
+
+  return true;
+};
+
+const uploadHeaders = {
+  authorization: `Bearer ${localStg.get('token')}`
 };
 </script>
 
 <template>
-  <ASelect :value="value" v-bind="{ ...fieldProps, prefix: undefined, options: undefined }" @change="updateValue">
-    <ASelectOption v-for="option in fieldProps?.options" :key="option" :value="option">
-      <div class="flex items-center gap-2">
-        <SvgIcon :icon="option" />
-        {{ option.replace('ant-design:', '').replace('-outlined', '') }}
-      </div>
-    </ASelectOption>
-  </ASelect>
+  <AUpload
+    :action="action"
+    :file-list="getFileList || value"
+    :multiple="true"
+    name="file"
+    :before-upload="checkUpload"
+    :headers="uploadHeaders"
+    @change="onFileListChange"
+  >
+    <AButton :icon="uploadIcon">{{ button }}</AButton>
+  </AUpload>
 </template>
 
-<style scoped></style>
+<style scoped>
+/* Custom styles */
+</style>
