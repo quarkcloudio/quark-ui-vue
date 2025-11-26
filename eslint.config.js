71b02ee0d9dabd2628e72cbf731337783cbbc7d0
tangtanglove <dai_hang_love@126.com>
2025-11-26 10:51:25 +0800

    feat: 增加Editor组件

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from '@soybeanjs/eslint-config';
 
 export default defineConfig(
-  { vue: true, unocss: true },
+  { vue: true, ignores: ['public/*'], unocss: true },
   {
     rules: {
       'vue/multi-word-component-names': [
---
+++ src/components/pro-form/pro-field/pro-form-editor.vue
@@ -0,0 +1,41 @@
+<script setup>
+import Editor from '@tinymce/tinymce-vue';
+defineOptions({
+  name: 'ProFormEditor'
+});
+</script>
+
+<template>
+  <main id="sample">
+    <Editor
+      id="uuid"
+      license-key="gpl"
+      :init="{
+        language: 'zh_CN',
+        promotion: false,
+        branding: false,
+        plugins:
+          'advlist anchor autolink charmap code fullscreen help image insertdatetime link lists media preview searchreplace table visualblocks wordcount',
+        toolbar:
+          'undo redo | styles | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+        height: 500
+      }"
+    />
+  </main>
+</template>
+
+<style scoped>
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  #sample {
+    display: flex;
+    flex-direction: column;
+    place-items: center;
+    width: 1000px;
+  }
+}
+</style>
